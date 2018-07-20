4d1ebdc6e9d5683f5df72e41319e95db16ce483f
Emmie Hayes <emmiehayes2@gmail.com>
2018-07-20 10:28:44 -0600

    build out next feature

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -25,12 +25,14 @@ feature 'When a user visits a vending machine show page' do
     expect(page).to have_content("$1")
   end
 
-  xscenario 'they see avg price for all items in the machine' do
+  scenario 'they see avg price for all items in the machine' do
     owner = Owner.create(name: "Sam's Snacks")
     dons  = owner.machines.create(location: "Don's Mixed Drinks")
+    sprite = dons.items.create(name: 'Sprite', price: 1)
+    coke = dons.items.create(name: 'CocaCola', price: 3)
 
     visit machine_path(dons)
 
-    expect(page).to have_content("Don's Mixed Drinks Vending Machine")
+    expect(page).to have_content("Average Price: $2")
   end
 end
